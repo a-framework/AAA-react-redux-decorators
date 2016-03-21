a4c0d76f778b22d76feddb55a823a25f7f461099
Dmitry Nutels <dnutels@gmail.com>
2016-03-21 09:27:46 +0200

    0.0.5

--- src/container.js
+++ src/container.js
@@ -1,36 +1,53 @@
 import React, {Component} from 'react';
+import {IntlProvider, injectIntl} from 'react-intl';
 
-export default function Container(options) {
+export const decorateContainer = function(DecoratedComponent, options = {}) {
+    const ConnectedDecoratedComponent = injectIntl(DecoratedComponent);    
+
+    const Container = class extends Component {
+        static get propTypes() {
+            return options.propTypes;
+        }
+
+        dispatch(actionCreator) {
+            actionCreator((action) => {
+                this.setState(action.payload || {});
+            });
+        }
+
+        render() {
+            const {dispatch = this.dispatch} = this.props;
+            const {locale = 'en'} = options;
+            
+            return (  
+                <IntlProvider locale={locale}>
+                    <ConnectedDecoratedComponent 
+                        {...this.props} 
+                        {...this.state} 
+                        dispatch={dispatch.bind(this)}
+                    />
+                </IntlProvider>
+            );
+        }
+    };
+
+    Object.defineProperty(Container, 'name', {writable: true});
+    Container.name = DecoratedComponent.name;
+    Object.defineProperty(Container, 'name', {writable: false});
+
+    return Container;
+};
+
+export default function ContainerDecorator(options = {}) {
     // `options` is the class being decorated
-    let result = options;
+    let result;
 
     if (typeof options !== 'function') {
-        result = (DecoratedComponent) => {
-            return class Decorator extends Component {
-                static get propTypes() {
-                    return options.propTypes;
-                }
-
-                dispatch(actionCreator) {
-                    actionCreator((action) => {
-                        this.setState(action.payload || {});
-                    });
-                }
-
-                render() {
-                    const {dispatch = this.dispatch} = this.props;
-
-                    return (  
-                        <DecoratedComponent 
-                            {...this.props} 
-                            {...this.state} 
-                            dispatch={dispatch.bind(this)}
-                        />
-                    );
-                }
-            };        
-        };        
+        result = (DecoratedComponent) => decorateContainer(DecoratedComponent, options);
+    } else {
+        result = decorateContainer(options);
     }
 
     return result;
-};
+}
+
